fcbe5a036f08cee2fc627565da56e23e7c8a7844
tymek <tymekbiziuk@gmail.com>
2025-10-20 15:00:10 +0200

    Init and a bit more and a bit more

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ app.use(express.json()) // Adding middleware to parse JSON bodies
 
 const router = express.Router()
 router.get('/', (req, res) => res.send('Hello World!'))
-router.get('/users', (req, res) => res.json([
-]))
 
 app.use('/api', router)
 
