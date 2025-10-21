b74dd0f159e232bf5315ab527bd33f4b94bb2335
tymek <tymekbiziuk@gmail.com>
2025-10-21 15:15:03 +0200

    ts configuration

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ app.use(express.json()) // Adding middleware to parse JSON bodies
 const router = express.Router()
 router.get('/', (req, res) => res.send('Hello World!'))
 
-app.use('/api', router)
+app.use('/', router)
 
 app.listen(8000) // Start the server on port 8000
