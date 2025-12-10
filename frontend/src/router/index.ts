cdd3a990ccda19fdb92afc1f13f13db8c1bdd4dc
mati <mateuszzaniewski1@gmail.com>
2025-12-10 23:11:32 +0100

    add: fetching not found problems

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth' // <--- 1. IMPORTUJEMY STORE
+
 import HomeView from '@/views/HomeView.vue'
-import UserDashboard from '@/views/user/UserDashboard.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 export const router = createRouter({
     history: createWebHistory(),
@@ -19,11 +21,13 @@ export const router = createRouter({
 
         {
             path: '/panel',
+            // 2. OZNACZAMY TĘ TRASĘ JAKO CHRONIONĄ
+            meta: { requiresAuth: true },
             component: () => import('@/views/user/UserLayout.vue'),
 
             children: [
                 {
-                    path: '', // Pusta ścieżka = domyślny widok po wejściu na /panel
+                    path: '',
                     name: 'user-dashboard',
                     component: () => import('@/views/user/UserDashboard.vue')
                 },
@@ -59,8 +63,26 @@ export const router = createRouter({
             path: '/leaderboard',
             name: 'leaderboard',
             component: () => import('@/components/sections/LeaderboardSection.vue')
+        },
+
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: NotFoundView
         }
     ]
 })
 
+router.beforeEach(async (to, from, next) => {
+    const auth = useAuthStore()
 
+    if (!auth.booted) {
+        auth.hydrateFromStorage()
+    }
+
+    if (to.meta.requiresAuth && !auth.isAuthenticated) {
+        next('/home')
+    } else {
+        next()
+    }
+})
